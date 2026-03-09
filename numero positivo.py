a57a6ed9b4ff61b98f5faa8c600083c3b1db067e
odiias <matheus.dias.galvao@escola.pr.gov.br>
2026-03-09 09:21:37 -0300

    Fix print statements and input prompt in script

--- numero positivo.py
+++ numero positivo.py
@@ -1,11 +1,11 @@
-numero = float(input("Digite um número: "))
+numero = float(input("Digite um numero: "))
 valida_pos = numero > 0
 valida_neg = numero < 0
 valida_zero = numero == 0
 
 if valida_pos:
-    print("O número é positivo.")
+    print("O numero e positivo.")
 elif valida_neg:
-    print("O número é negativo.")
+    print("O numero e negativo.")
 elif valida_zero:
-    print("O número é zero.")
+    print("O numero e zero.")
