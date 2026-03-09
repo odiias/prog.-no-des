3a586619456e16f52bfd11d272f0e7ec09a65721
odiias <matheus.dias.galvao@escola.pr.gov.br>
2026-03-09 09:19:37 -0300

    Refactor number validation conditions for clarity

--- numero positivo.py
+++ numero positivo.py
@@ -1,8 +1,11 @@
 numero = float(input("Digite um número: "))
+valida_pos = numero > 0
+valida_neg = numero < 0
+valida_zero = numero == 0
 
-if numero > 0:
+if valida_pos:
     print("O número é positivo.")
-elif numero < 0:
+elif valida_neg:
     print("O número é negativo.")
-else:
+elif valida_zero:
     print("O número é zero.")
