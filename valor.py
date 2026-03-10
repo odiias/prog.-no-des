58ab3d068684e3fabafc169de29b8f9e8ca5649d
odiias <matheus.dias.galvao@escola.pr.gov.br>
2026-03-10 08:41:04 -0300

    Add discount validation for purchase value

--- valor.py
+++ valor.py
@@ -1,6 +1,7 @@
 valor_compra = float(input("Digite o valor da compra: R$ "))
+valida_desconto= valor_compra >= 100
 
-if valor_compra >= 100:
+if valida_desconto:
     desconto = valor_compra * 0.10
     valor_final = valor_compra - desconto
     print(f"Valor com desconto: R$ {valor_final:.2f}")
