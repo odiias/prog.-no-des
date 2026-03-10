10e67fc7c9a4d07ebf6307ab3c9c3cb0d46d5882
odiias <matheus.dias.galvao@escola.pr.gov.br>
2026-03-10 08:31:40 -0300

    Refactor age check logic for clarity

--- maior de idade.py
+++ maior de idade.py
@@ -1,6 +1,7 @@
-numero = float(input("Digite sua idade: "))
+idade = float(input("Digite sua idade: "))
+valida_idade = idade >= 18
 
-if numero >= 18:
+if valida_idade:
     print("Você é maior de idade")
 else:
     print("Você é menor de idade.")
